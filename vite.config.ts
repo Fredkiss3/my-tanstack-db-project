54e300f6331820cd786928c281bace6072b29c61
Fred KISSIE <fredkiss3@gmail.com>
2025-07-30 22:45:23 +0200

    🎉 initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import viteTsConfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite"
+
+const config = defineConfig({
+  server: {
+    port: 5174
+  },
+  plugins: [
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: [`./tsconfig.json`],
+    }),
+    tailwindcss(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
+  ],
+})
+
+export default config
